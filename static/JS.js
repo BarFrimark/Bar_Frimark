9f75b04fd0336e19baa6b1d51c92295c05e2ce75
BarFrimark <barfrimark@gmail.com>
2022-09-11 13:15:49 +0300

    new 11.9 13:15

--- static/JS.js
+++ static/JS.js
@@ -1,3 +1,18 @@
+function Greeting() {
+    console.log("im in my greeting");
+    var time = new Date().getUTCHours();
+    let greeting;
+    if (time < 11) {
+        greeting = "Good morning Bar";
+    } else if (time < 18) {
+        greeting = "Good Day Bar";
+    } else {
+        greeting = "Good evening Bar";
+    }
+    document.getElementById("greet").innerHTML = greeting;
+};
+
+
 function StartEndShift(){
     if(document.getElementById("start__button").innerHTML == "Start New Shift"){
         document.getElementById("start__button").innerHTML = "End This Shift";
@@ -24,9 +39,17 @@ function showtable1() {
 function validPassword()
 {
     var p = document.getElementById("newPassword").value; 
-    if(!p.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/)) {  
-         document.getElementById("passwordCheck").innerHTML = "Password must contain 8 characters, numbers, upper and lower case letters";  
+    if(p.length<8) {  
+         document.getElementById("passwordCheck").innerHTML = "Password must contain 8 characters";  
          return false;
+    }
+    if(!p.match(/(?=.*\d)/g)) {  
+        document.getElementById("passwordCheck").innerHTML = "Password must contain numbers";  
+        return false;
+    }
+    if(!p.match(/(?=.*[a-z])(?=.*[A-Z])/g)) {  
+        document.getElementById("passwordCheck").innerHTML = "Password must contain upper and lower case letters";  
+        return false;
     }
     return true;
 }
